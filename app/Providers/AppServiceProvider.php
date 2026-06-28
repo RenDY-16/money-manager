d01b6a3b6aa4e0e53bb29e8c07d994bde3d5ca52
RenDY-16 <refazym@gmail.com>
2026-06-29 06:18:42 +0800

    https tes 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (str_contains(config('app.url'), 'railway.app') || app()->environment('development')) {
             URL::forceScheme('https');
         }
     }
